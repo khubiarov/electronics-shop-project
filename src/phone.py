a5528a0bdc802136806cf9b5dfa2a19f93c64305
khubiarov <72666523+khubiarov@users.noreply.github.com>
2023-04-21 15:40:26 +0800

    I have started do homework 4, some things in process

---
+++ src/phone.py
@@ -0,0 +1,19 @@
+from src.item import Item
+
+
+class Phone(Item):
+
+    def __init__(self, name: str, price: float, quantity: int, number_of_sim):
+        super().__init__(name, price, quantity)
+        self.number_of_sim = number_of_sim
+
+
+    def __add__(self, other):
+        if isinstance(other, Item):
+            return self.quantity + other.quantity
+        else:
+            raise TypeError
+
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}('{self.name}', {self.price}, {self.quantity}, {self.number_of_sim})"
