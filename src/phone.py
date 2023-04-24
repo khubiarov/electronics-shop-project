4ebcdda4b7670f0cac95bb205c87c46372e1a9f6
khubiarov <72666523+khubiarov@users.noreply.github.com>
2023-04-25 06:56:27 +0800

    homework 4 , upd

--- src/phone.py
+++ src/phone.py
@@ -7,13 +7,27 @@ class Phone(Item):
         super().__init__(name, price, quantity)
         self.number_of_sim = number_of_sim
 
-
     def __add__(self, other):
         if isinstance(other, Item):
             return self.quantity + other.quantity
         else:
             raise TypeError
 
-
     def __repr__(self):
         return f"{self.__class__.__name__}('{self.name}', {self.price}, {self.quantity}, {self.number_of_sim})"
+
+    @property
+    def sim(self):
+        return self.number_of_sim
+
+    @sim.setter
+    def sim(self, value):
+
+        if value > 0 and value.is_iteger():
+            self.number_of_sim = value
+
+        else:
+
+             raise ValueError('ОШибка')
+
+
